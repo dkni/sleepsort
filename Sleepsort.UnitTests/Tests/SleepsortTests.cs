1c4f786178f889fd5adc451e45d315b636d79bc1
Aliaksandr Kastsiukevich <Aliaksandr_Kastsiukevich@epam.com>
2018-12-03 14:09:28 +0300

    add plinq sort method and its unit tests

--- Sleepsort.UnitTests/Tests/SleepsortTests.cs
+++ Sleepsort.UnitTests/Tests/SleepsortTests.cs
@@ -6,35 +6,68 @@ namespace Sleepsort.UnitTests.Tests
     class SleepsortTests
     {
         [Test]
-        public void SleepSort_Sort_ReturnsSortedArray()
+        public void SleepSort_TplSort_ReturnsSortedArray()
         {
             var sorter = new Sleepsort();
 
             var testData =       new[] { 1, 7, 6, 3, 5, 9, 0, 2, 4, 8 };
             var expectedResult = new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            var result = sorter.Sort(testData);
+            var result = sorter.TplSort(testData);
             Assert.AreEqual(result, expectedResult);
         }
 
         [Test]
-        public void SleepSort_Sort_ReturnsSortedArray1()
+        public void SleepSort_TplSort_ReturnsSortedArray1()
         {
             var sorter = new Sleepsort();
 
             var testData1 = new[] { 11, 71, 61, 31, 51, 91, 0, 21, 41, 81 };
             var expectedResult1 = new[] { 0, 11, 21, 31, 41, 51, 61, 71, 81, 91 };
-            var result1 = sorter.Sort(testData1);
+            var result1 = sorter.TplSort(testData1);
             Assert.AreEqual(result1, expectedResult1);
         }
 
         [Test]
-        public void SleepSort_Sort_ReturnsSortedArray2()
+        public void SleepSort_TplSort_ReturnsSortedArray2()
         {
             var sorter = new Sleepsort();
 
             var testData2 = new[] { 23, 74, 16, 31, 51, 55, 14, 33, 42, 19 };
             var expectedResult2 = new[] { 14, 16, 19, 23, 31, 33, 42, 51, 55, 74 };
-            var result2 = sorter.Sort(testData2);
+            var result2 = sorter.TplSort(testData2);
+            Assert.AreEqual(result2, expectedResult2);
+        }
+
+        [Test]
+        public void SleepSort_PlinqSort_ReturnsSortedArray()
+        {
+            var sorter = new Sleepsort();
+
+            var testData = new[] { 1, 7, 6, 3, 5, 9, 0, 2, 4, 8 };
+            var expectedResult = new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var result = sorter.PlinqSort(testData);
+            Assert.AreEqual(result, expectedResult);
+        }
+
+        [Test]
+        public void SleepSort_PlinqSort_ReturnsSortedArray1()
+        {
+            var sorter = new Sleepsort();
+
+            var testData1 = new[] { 11, 71, 61, 31, 51, 91, 0, 21, 41, 81 };
+            var expectedResult1 = new[] { 0, 11, 21, 31, 41, 51, 61, 71, 81, 91 };
+            var result1 = sorter.PlinqSort(testData1);
+            Assert.AreEqual(result1, expectedResult1);
+        }
+
+        [Test]
+        public void SleepSort_PlinqSort_ReturnsSortedArray2()
+        {
+            var sorter = new Sleepsort();
+
+            var testData2 = new[] { 23, 74, 16, 31, 51, 55, 14, 33, 42, 19 };
+            var expectedResult2 = new[] { 14, 16, 19, 23, 31, 33, 42, 51, 55, 74 };
+            var result2 = sorter.PlinqSort(testData2);
             Assert.AreEqual(result2, expectedResult2);
         }
     }
--- Sleepsort/src/Sleepsort.cs
+++ Sleepsort/src/Sleepsort.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -6,7 +7,7 @@ namespace Sleepsort
 {
     public class Sleepsort
     {
-        public int[] Sort(IEnumerable<int> source)
+        public int[] TplSort(IEnumerable<int> source)
         {
             List<int> output = new List<int>();
             Parallel.ForEach(source, number =>
@@ -14,6 +15,21 @@ namespace Sleepsort
                 Thread.Sleep(100 * number);
                 output.Add(number);
             });
+            return output.ToArray();
+        }
+
+        public int[] PlinqSort(IEnumerable<int> source)
+        {
+            List<int> output = new List<int>();
+            var fixedSource = source.ToArray();
+            fixedSource.AsParallel()
+                .WithDegreeOfParallelism(fixedSource.Length)
+                .ForAll(number =>
+                {
+                    Thread.Sleep(100 * number);
+                    output.Add(number);
+                }
+                );
             return output.ToArray();
         }
     }
